d89975d7ddb972b6d0d2f65bdbce38d3735429ca
eve <eve@kali.kali>
2020-06-23 09:53:16 +0200

    Fixing env restore

--- restore_env_bck.sh
+++ restore_env_bck.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 BCK_FNAME=$1
-cantreadth1s $BCK_FNAME --outfile bck.zip
-unzip bck.zip
-rm bck.zip
-cd env
+OUTDIR=$PWD
+tmpdir=$(mktemp -d)
+cantreadth1s $BCK_FNAME --outfile $tmpdir/bck.zip
+patchdir=$tmpdir/env/envbck/
+cd $tmpdir
+unzip bck.zip #$tmpdir/bck.zip -d $tmpdir
+#rm -rf ./env &&
+mkdir env && cd env
 git init
-echo "$(ls envbck)" > list_envs
-git add *
+echo "$(ls $patchdir)" > list_envs
+git add * #list_envs create_env .quit_env
 git commit -m "Loading bck $BCK_FNAME"
-for e in $(ls envbck);
+for e in $(ls $patchdir);
 do
     git branch $e
     git checkout $e
-    git apply envbck/$e/*.patch
+    for f in $(ls $patchdir/$e/*.patch);
+    do
+        git apply --whitespace=nowarn $f
+    done
     git add *
     git commit -m "Applied patches from bck"
     git checkout master
 done
-rm -r envbck
+echo $tmpdir
+rm bck.zip
+cp -r $tmpdir/* $OUTDIR
